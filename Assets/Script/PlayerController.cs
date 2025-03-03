740604f63dc26e9cee193be6318f09d0b20c2c50
jeel11p <jeel11patel@gmail.com>
2025-03-02 20:16:38 -0800

    Implemented Double-Jump Feature

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,52 +1,44 @@
 using UnityEngine;
 
-
 public class PlayerController : MonoBehaviour
 {
-   public float speed = 5.0f;
-   private float horizontalInput;
-   private float forwardInput;
-   public float jumpForce = 8f;
-   private Rigidbody rb;
-   private bool isGrounded = true;
-   public Transform cameraTransform; 
+    public float speed = 5.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public float jumpForce = 8f;
+    private Rigidbody rb;
+    private bool isGrounded = true;
+    private int jumpCount = 0; 
+    public int maxJumps = 2;  
+    public Transform cameraTransform;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
-   void Start()
-   {
-       rb = GetComponent<Rigidbody>();
-   }
+        // Check if the player presses jump and has remaining jumps
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumps)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpCount++;
+        }
+    }
 
-
-   void Update()
-   {
-       horizontalInput = Input.GetAxis("Horizontal");
-       forwardInput = Input.GetAxis("Vertical");   
-  
-       transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-       transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
-
-
-       if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-       {
-           rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-           isGrounded = false;
-       }
-   }
-
-
-   private void OnCollisionEnter(Collision collision)
-   {
-       if (collision.gameObject.CompareTag("Ground"))
-       {
-           isGrounded = true;
-       }
-   }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            jumpCount = 0;  // Reset jump count when touching the ground
+        }
+    }
 }
-
-
-
-
-
